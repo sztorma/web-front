dbf5fa61bf15041871f110a6fdb5da530c56e444
Sztorma <sztorma@outlook.com>
2019-02-05 16:03:12 +0100

    front preliminary deploy

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,22 @@
           <v-toolbar-title>TormaSzabolcs</v-toolbar-title>
           <v-spacer></v-spacer>
           <v-toolbar-items class="hidden-sm-and-down">
-              <v-btn class="white--text" flat to="/">Main</v-btn>
-              <v-btn class="white--text" flat to="/projects">Projects</v-btn>
+              <v-btn class="white--text" 
+              v-for="item in menu"
+              :key="item.icon"
+              :to="item.link" 
+              flat>{{item.title}}</v-btn>
           </v-toolbar-items>
+          <v-menu class="hidden-md-and-up">
+            <v-toolbar-side-icon slot="activator" class="white--text"></v-toolbar-side-icon>
+
+            <v-list>
+              <v-list-tile v-for="item in menu" :key="item.title" :to="item.link">
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile>   
+            </v-list>            
+
+          </v-menu>
       </v-toolbar>
     <router-view class="mb-4"/>
       <section>
@@ -24,8 +37,19 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'Appppp',
+
+  data () {
+    return {
+      menu: [
+        { link: '/', title: 'Home' },
+        { link: '/projects', title: 'Projects' },
+      ]
+    }
+  },
+  
+
+};
 </script>
 
 <style>
