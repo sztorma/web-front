232ac2839058e87ab16bec49bb74290eb734db8b
Sztorma <sztorma@outlook.com>
2019-02-08 17:18:20 +0100

    prod URL change

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"https://git.heroku.com/db-manager-spring.git"'
+  ROOT_API: '"https://db-manager-spring.herokuapp.com/"'
 }
